0907c3483a7ef4cca7130646a616aeb08708068b
Aaron Wald <aaron.wald@docusign.com>
2014-05-02 08:54:25 -0700

    take -u and -p params

--- deploy/deploy-monster-scoring.ps1
+++ deploy/deploy-monster-scoring.ps1
@@ -1,3 +1,9 @@
+param( 	[string]$u = "", [string]$p = "" )
+if ("$u" -eq "" -or "$p" -eq "") {
+    "Please set -u and -p"
+    exit;
+}
+
 $startpath = get-location
 $outpath = "/monstergolf/monsterscoring"
 $dir = "../.." + $outpath
@@ -9,10 +15,8 @@ C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\aspnet_compiler.exe -v /onlinescor
 set-location $dir
 $location = get-location
 $ftp = "ftp://97.74.215.204"+$outpath 
-$user = "" 
-$pass = ""  
 $webclient = New-Object System.Net.WebClient 
-$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)  
+$webclient.Credentials = New-Object System.Net.NetworkCredential($u,$p)  
 $files =  Get-ChildItem -Recurse
     
 foreach($file in $files)
--- deploy/deploy-monster-web.ps1
+++ deploy/deploy-monster-web.ps1
@@ -1,3 +1,8 @@
+param( 	[string]$u = "", [string]$p = "" )
+if ("$u" -eq "" -or "$p" -eq "") {
+    "Please set -u and -p"
+    exit;
+}
 $startpath = get-location
 $outpath = "/monstergolf"
 $dir = "../monster-golf-web/" + $outpath
@@ -6,10 +11,8 @@ $dir = "../monster-golf-web/" + $outpath
 set-location $dir
 $location = get-location
 $ftp = "ftp://97.74.215.204"+$outpath 
-$user = "mtsi5553" 
-$pass = "St3v3W@ld"  
 $webclient = New-Object System.Net.WebClient 
-$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)  
+$webclient.Credentials = New-Object System.Net.NetworkCredential($u,$p)  
 $files = Get-ChildItem -Recurse | Where-Object  {
    $_.LastWriteTime.ToShortDateString() -eq (Get-Date).ToShortDateString() 
 }
@@ -29,7 +32,7 @@ foreach($file in $files)
         $ftp_command = $ftp + $directory + $file
         $uri = New-Object System.Uri($ftp_command)
         "updating $uri from $source" 
-        $webclient.UploadFile($uri, $source)
+        #$webclient.UploadFile($uri, $source)
     }
 }
 
